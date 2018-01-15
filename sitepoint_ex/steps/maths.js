eebcdb44ce0a4e51106a4b32d9254cbb96329b4f
Dragan Nikolic <dragan.d.nikolic@gmail.com>
2018-01-15 12:08:07 +0100

    add multiplication

--- sitepoint_ex/steps/maths.js
+++ sitepoint_ex/steps/maths.js
@@ -1,16 +1,22 @@
 const defineSupportCode = require('cucumber').defineSupportCode;
+const assert = require('assert');
 
 defineSupportCode(function({ Given, Then, When }) {
-  Given('I start with {int}', function (int, callback) {
-    // Write code here that turns the phrase above into concrete actions
-    callback(null, 'pending');
+  let answer = 0;
+
+  Given('I start with {int}', function (input) {
+    answer = input;
   });
-  When('I add {int}', function (int, callback) {
-    // Write code here that turns the phrase above into concrete actions
-    callback(null, 'pending');
+
+  When('I add {int}', function (input) {
+    answer = answer + input;
   });
-  Then('I end up with {int}', function (int, callback) {
-    // Write code here that turns the phrase above into concrete actions
-    callback(null, 'pending');
+
+  When('I multiply by {int}', function (input) {
+    answer = answer * input;
+  });
+
+  Then('I end up with {int}', function (input) {
+    assert.equal(answer, input);
   });
 });
