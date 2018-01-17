8a383ea85b587ac285598e72036ba8c629093c5c
Dragan Nikolic <dragan.d.nikolic@gmail.com>
2018-01-18 00:32:02 +0100

    add http post request example using trae

--- sitepoint_ex/features/tt_songs_trae.feature
+++ sitepoint_ex/features/tt_songs_trae.feature
@@ -2,3 +2,9 @@ Feature: TabTracker
   Scenario: Get all songs
     When I make TT trae call to get all songs
     Then I should see all the songs in the database
+
+  Scenario: Create a song
+    When I add new song
+    Then I should see the same song in the database 
+
+  Scenario: Update a song
--- sitepoint_ex/steps/tab_tracker_trae.js
+++ sitepoint_ex/steps/tab_tracker_trae.js
@@ -17,5 +17,50 @@ defineSupportCode(function({ Given, Then, When }) {
       console.log(err);
       console.log('------------------------------------');
     }
-  })
+  });
+
+  When('I add new song', async function () {
+    let response;
+    try {
+      response = await request.post(
+        'http://localhost:8081/songs',
+        {
+          title: 'Kad hodas',
+          artist: 'Riblja Corba',
+          genre: 'Rock',
+          album: 'Veceras vas zabavljaju muzicari koji piju',
+          albumImageUrl: 'https://upload.wikimedia.org/wikipedia/en/b/b8/Muzicarikojipijiu.jpg',
+          youtubeId: '79rN-2a13Tc',
+          lyrics: 'mesec je bleda fleka boje cimeta',
+          tab: 'C       Em        G         Am'
+        });
+      this.new_song = response.data;
+      console.log('-----add new song data-------------------------------');
+      console.log(this.new_song);
+      console.log('------------------------------------');
+    } catch(err) {
+      console.log('-----error-------------------------------');
+      console.log(err);
+      console.log('------------------------------------');
+    }
+
+    assert.equal(response.status, 200, `Incorrect status code - ${response.status}`);
+  });
+
+  Then('I should see the same song in the database', async function () {
+    let response;
+    try {
+      response =  await request.get(`http://localhost:8081/songs/${this.new_song.id}`);
+      this.song = response.data;
+      console.log('----verify song--------------------------------');
+      console.log(this.song.title);
+      console.log('------------------------------------');
+    } catch(err) {
+      console.log('-----error-------------------------------');
+      console.log(err);
+      console.log('------------------------------------');
+    }  
+    assert.equal(response.status, 200, 'Incorrect status code');
+    assert.equal(this.song.title, this.new_song.title, 'incorrect song title');
+  });
 });
