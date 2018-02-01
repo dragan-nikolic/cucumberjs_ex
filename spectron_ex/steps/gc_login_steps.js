47aa54e4ee61d417b4d50a2d59b32dc93a245961
Dragan Nikolic <dragan.d.nikolic@gmail.com>
2018-02-01 18:05:15 +0100

    add spectron example - initial version

---
+++ spectron_ex/steps/gc_login_steps.js
@@ -0,0 +1,21 @@
+const defineSupportCode = require('cucumber').defineSupportCode;
+const Application = require('spectron').Application;
+const assert = require('assert');
+
+defineSupportCode(function({ Given, Then, When }) {
+  When('I start GameClient', async function () {
+    this.app = new Application({
+      path: 'C:\\Program Files\\GameCredits\\Client\\0.7\\gc-client.exe',
+    });
+    await this.app.start();
+  });
+
+  Then('I should see GameClient app open', async function () {
+  });
+
+  When('I enter email {string} and password {string}', async function (email, password) {
+  });
+
+  Then('I should see the user is logged in', function () {
+  });
+});
