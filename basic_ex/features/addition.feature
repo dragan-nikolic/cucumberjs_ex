9075f53e0e06a1a0db9a1b817d7b9affdb7742ed
Dragan Nikolic <dragan.d.nikolic@gmail.com>
2018-03-14 13:10:18 +0100

    add hooks to basic exmaple

--- basic_ex/features/addition.feature
+++ basic_ex/features/addition.feature
@@ -1,9 +1,11 @@
 Feature: Addition
+  @mytag
   Scenario: 1 + 0
     Given I start with 1
     When I add 0
     Then I end up with 1
 
+  @mytag
   Scenario: 1 + 1
     Given I start with 1
     When I add 1
--- basic_ex/steps/maths.js
+++ basic_ex/steps/maths.js
@@ -2,25 +2,23 @@ const defineSupportCode = require('cucumber').defineSupportCode;
 const assert = require('assert');
 
 defineSupportCode(function({ Given, Then, When }) {
-  let answer = 0;
-
   Given('I start with {int}', function (input) {
-    answer = input;
+    this.answer = input;
   });
 
   When('I add {int}', function (input) {
-    answer = answer + input;
+    this.answer = this.answer + input;
   });
 
   When('I substruct {int}', function (input) {
-    answer = answer - input;
+    this.answer = this.answer - input;
   });
 
   When('I multiply by {int}', function (input) {
-    answer = answer * input;
+    this.answer = this.answer * input;
   });
 
   Then('I end up with {int}', function (input) {
-    assert.equal(answer, input);
+    assert.equal(this.answer, input);
   });
 });
