a0d9b71f130cd087dd5d96d5b945e519de263d4c
Dragan Nikolic <dragan.d.nikolic@gmail.com>
2018-08-28 18:07:47 +0200

    reorginize cucumber and cheese

---
+++ cucumberjs_and_cheese/03_dipping/apple.js
@@ -0,0 +1,18 @@
+(async function() {
+// async wrapper begin
+
+const webdriverio = require('webdriverio')
+
+// create  webdriver
+const options = { 
+  desiredCapabilities: { 
+    browserName: 'chrome'
+  } 
+}
+
+const browser = webdriverio.remote(options)
+await browser.init()
+await browser.url('http:/www.apple.com')
+
+// async wrapper end
+})()
