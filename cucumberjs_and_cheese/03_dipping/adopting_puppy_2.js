dcad204b9270226f75d445fcd00fac1d8241412b
Dragan Nikolic <dragan.d.nikolic@gmail.com>
2018-08-30 14:42:48 +0200

    refactor adopt_puppy_2

--- cucumberjs_and_cheese/03_dipping/adopting_puppy_2.js
+++ cucumberjs_and_cheese/03_dipping/adopting_puppy_2.js
@@ -3,38 +3,44 @@ Run: $ node <file>.js
 */
 
 (async function() {
-// async wrapper begin
+  // async wrapper begin
+  
+  const webdriverio = require('webdriverio')
+  
+  async function clickOnItem(browser, selector, index) {
+    const elements = await browser.elements(selector)
+    await browser.elementIdClick(elements.value[index].ELEMENT)
+  }
+  
+  async function adoptPuppyNumber(browser, number) {
+    await clickOnItem(browser, '[value="View Details"]', number)
+    await browser.click('[value="Adopt Me!"]')
+  }
 
-const webdriverio = require('webdriverio')
-
-// create  webdriver
-const options = { 
-  desiredCapabilities: { 
-    browserName: 'chrome'
-  } 
-}
-
-const browser = webdriverio.remote(options)
-await browser.init()
-await browser.url('http:/puppies.herokuapp.com')
-
-const puppies = await browser.elements('[value="View Details"]')
-console.log('puppies', puppies)
-for (var p in puppies) {
-  console.log(p)
-}
-console.log('p', await browser.elementIdClick(puppies.value[1].ELEMENT))
-// await browser.click('[value="View Details"]')
-// await browser.click('[value="Adopt Me!"]')
-// await browser.click('[value="Complete the Adoption"]')
-// await browser.setValue('#order_name', 'Cheese')
-// await browser.setValue('#order_address', '123 Main St.')
-// await browser.setValue('#order_email', 'cheese@foo.com')
-// await browser.selectByValue('#order_pay_type', 'Check')
-// await browser.click('[value="Place Order"]')
-// await browser.waitForExist('#notice=Thank you for adopting a puppy!', 5000)
-
-//await browser.end()
-
-// async wrapper end
-})()
+  // create  webdriver
+  const options = { 
+    desiredCapabilities: { 
+      browserName: 'chrome'
+    } 
+  }
+  
+  const browser = webdriverio.remote(options)
+  await browser.init()
+  await browser.url('http:/puppies.herokuapp.com')
+  
+  await adoptPuppyNumber(browser, 1)
+  await browser.click('[value="Adopt Another Puppy"]')
+  await adoptPuppyNumber(browser, 2)
+  await browser.click('[value="Complete the Adoption"]')
+  await browser.setValue('#order_name', 'Cheese')
+  await browser.setValue('#order_address', '123 Main St.')
+  await browser.setValue('#order_email', 'cheese@foo.com')
+  await browser.selectByValue('#order_pay_type', 'Check')
+  await browser.click('[value="Place Order"]')
+  await browser.waitForExist('#notice=Thank you for adopting a puppy!', 5000)
+  
+  await browser.end()
+  
+  // async wrapper end
+  })()
+  
